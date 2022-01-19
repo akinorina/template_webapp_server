2a169297a779836cd73380df29499626a5c511e7
Akinori Nakata <nakata@real-its.com>
2022-01-19 12:09:24 +0900

    seeder を migration として再実装。

--- src/migration/1642558198619-User.ts
+++ src/migration/1642558198619-User.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class User1642499286981 implements MigrationInterface {
-    name = 'User1642499286981'
+export class User1642558198619 implements MigrationInterface {
+    name = 'User1642558198619'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL DEFAULT '', \`nameKana\` varchar(255) NOT NULL DEFAULT '', \`email\` varchar(255) NOT NULL DEFAULT '', \`password\` varchar(255) NOT NULL DEFAULT '', \`userType\` varchar(255) NOT NULL DEFAULT 'customer', \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
---
+++ src/migration/1642558369576-User.ts
@@ -0,0 +1,27 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class User1642558369576 implements MigrationInterface {
+    name = 'User1642558369576'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `INSERT \`user\` SET
+                \`id\` = 1,
+                \`name\` = "管理者",
+                \`nameKana\` = "かんりしゃ",
+                \`email\` = "admin@example.com",
+                \`password\` = "admin",
+                \`userType\` = "admin",
+                \`createdAt\` = DEFAULT,
+                \`updatedAt\` = DEFAULT,
+                \`deletedAt\` = DEFAULT`
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `DELETE FROM \`user\` WHERE id = 1`
+        );
+    }
+
+}
