c66c86c1890a68234201180aa495101ebaa58fd2
Akinori Nakata <nakata@real-its.com>
2022-04-07 00:45:28 +0900

    mariadb 前提で再度 migration 生成・実行

--- src/migration/1649259739721-User.ts
+++ src/migration/1649259739721-User.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class User1649144395322 implements MigrationInterface {
-    name = 'User1649144395322'
+export class User1649259739721 implements MigrationInterface {
+    name = 'User1649259739721'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT COMMENT 'ユーザーID', \`name\` varchar(255) NOT NULL COMMENT '氏名' DEFAULT '', \`nameKana\` varchar(255) NOT NULL COMMENT '氏名かな' DEFAULT '', \`email\` varchar(255) NOT NULL COMMENT 'Emailアドレス' DEFAULT '', \`password\` varchar(255) NOT NULL COMMENT 'パスワード' DEFAULT '', \`userType\` varchar(255) NOT NULL COMMENT 'ユーザー種別 (admin|user)' DEFAULT 'user', \`createdAt\` timestamp(6) NOT NULL COMMENT '作成日時' DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` timestamp(6) NOT NULL COMMENT '更新日時' DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` timestamp(6) NULL COMMENT '削除日時', PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
