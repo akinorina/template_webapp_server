283728fbb1322d0e97bc46e08a16f53145a49e3b
Akinori Nakata <nakata@real-its.com>
2022-04-13 18:03:32 +0900

    entity更新に伴う修正（Webページ）

--- src/routes/management/image.ts
+++ src/routes/management/image.ts
@@ -14,7 +14,7 @@ import dataSource from '../../dataSource';
 import { Image } from '../../entity/Image'
 
 // console logger
-import consoleLogger from '../../lib/log/consoleLogger'
+// import consoleLogger from '../../lib/log/consoleLogger'
 
 // libs
 import { isAuthenticated } from '../../lib/libAuth'
@@ -35,14 +35,14 @@ const router = express.Router()
 router.get('/', isAuthenticated, async function (req: express.Request, res: express.Response, next: express.NextFunction) {
   // パラメータ取得
   const parameters = getAllParameters(req)
-  consoleLogger.debug('parameters', parameters)
+  // consoleLogger.debug('parameters', parameters)
 
   // パラメータ設定
   const options = { where: { userId: req.user.id } };
 
   // DB access
   const images = await dataSource.getRepository(Image).find(options)
-  consoleLogger.debug('--- images: ', images)
+  // consoleLogger.debug('--- images: ', images)
 
   // render page.
   const viewValues = { login_user: req.user, Images: images }
@@ -59,7 +59,7 @@ router.get('/new', isAuthenticated, function (req: express.Request, res: express
 })
 
 // ---
-// 管理画面 - 画像 - 編集画面からSUBMIT (新規)
+// 管理画面 - 画像 - 新規登録画面からSUBMIT
 // ---
 router.post('/', isAuthenticated, async function (req: express.Request, res: express.Response, next: express.NextFunction) {
   if (!req.user) {
@@ -79,28 +79,28 @@ router.post('/', isAuthenticated, async function (req: express.Request, res: exp
   // ---
   // ファイル保存、DB登録
   // ---
+
   // 単一の場合、配列に変換
-  let imagelist = (!Array.isArray(parameters.__files.images)) ? [ parameters.__files.images ] : parameters.__files.images;
-  imagelist.forEach(async (ele: any) => {
-    let bValid = true;
+  let imageList = (!Array.isArray(parameters.__files.images)) ? [ parameters.__files.images ] : parameters.__files.images;
+  imageList.forEach(async (ele: any) => {
+    // 画像UPLOADエラー
+    let errorMessages: Array<any> = [];
 
     // アップロードファイルは20MB以内
     if (ele.size > serverConfig.app.image_management.maxSize) {
-      // consoleLogger.error('ele.size too big! : ', ele.size);
-      bValid = false;
+      errorMessages.push({ filename: ele.name, message: 'image size is too big. : ' + ele.size })
     }
 
     // mimetype 対応確認
     if (!serverConfig.app.image_management.mimeType.includes(ele.mimetype)) {
-      // consoleLogger.error('ele.mimetype bad type! : ', ele.mimetype);
-      bValid = false;
+      errorMessages.push({ filename: ele.name, message: 'image mime_type is invalid: ' + ele.mimetype })
     }
 
-    if (bValid) {
+    if (errorMessages.length == 0) {
       // ---
       // ファイルを保存・配置
       // ---
-      // 保存するファイル名は同じファイル名が生じるケースを考えてDate.now()をつけたす
+      // 保存するファイル名は同じファイル名が生じるケースを考えて Date.now() をつけたす
       let uploadFileExt = path.extname(ele.name);
       let saveFilename = `${path.basename(ele.name, uploadFileExt)}-${Date.now()}${uploadFileExt}`;
 
@@ -110,8 +110,9 @@ router.post('/', isAuthenticated, async function (req: express.Request, res: exp
         // 保存先ディレクトリが無い場合は作成
         fs.mkdirSync(path.join(APP_ROOT, 'public', savedDir), { recursive: true, mode: 0o777 });
       }
+      // file path
       let filePath = `public/${savedDir}${saveFilename}`;
-      // URLパス
+      // URL path
       let urlPath = `/${savedDir}${saveFilename}`;
 
       // メモリ上にあるファイルをサーバーパスへ移動させる
@@ -126,7 +127,6 @@ router.post('/', isAuthenticated, async function (req: express.Request, res: exp
 
       // 生成データ作成
       const image = {
-        name: parameters.name,
         userId: req.user ? req.user.id : 0,
         fileName: path.basename(ele.name),
         fileMimetype: ele.mimetype,
@@ -142,6 +142,12 @@ router.post('/', isAuthenticated, async function (req: express.Request, res: exp
         .into(Image)
         .values(image)
         .execute()
+    } else {
+      let em = '';
+      errorMessages.forEach((ele) => {
+        em += (em === '' ? "" : "\n") + ele.message
+      })
+      // consoleLogger.error(em)
     }
   })
 
@@ -196,8 +202,16 @@ router.post('/:id', isAuthenticated, async function (req, res, next) {
 
   // パラメータ取得
   const parameters = getAllParameters(req);
+  if (!parameters.__files || !parameters.__files.images) {
+    // redirect page.
+    res.redirect(`/management/image/${parameters.id}`)
+    return;
+  }
+  // consoleLogger.debug('parameters', parameters);
 
+  // ---
   // IDによる対象データ取得
+  // ---
   const image = await dataSource.getRepository(Image).findOneBy({ id: parameters.id })
   if (image === null) {
     // redirect page.
@@ -205,73 +219,73 @@ router.post('/:id', isAuthenticated, async function (req, res, next) {
     return
   }
   // consoleLogger.debug('image: ', image)
-  
-  // 画像ID
+  // (既存)画像ID
   let image_id = image.id;
-  // 画像 mime type
+  // (既存)画像ID
+  let imageFilename = image.fileName;
+  // (既存)画像 mime type
   let fileMimetype = image.fileMimetype;
-  // サーバー上の保存位置
+  // (既存)サーバー上の保存位置
   let filePath = image.filePath;
-  // URLパス
+  // (既存)URLパス
   let urlPath = image.fileUrl;
 
-  // 画像ファイル指定がある場合はDBに画像を保存
-  if (parameters.__files && parameters.__files.images) {
-    // consoleLogger.debug('parameters', parameters);
+  // ---
+  // ファイル保存、DB登録
+  // ---
+  // 単一の場合、配列に変換。しかしながら、以下では[0]のデータのみ扱う
+  let imageList = (!Array.isArray(parameters.__files.images)) ? [ parameters.__files.images ] : parameters.__files.images;
+  // consoleLogger.debug('imageList', imageList);
+
+  // 画像UPLOADエラー
+  let errorMessages: Array<any> = [];
+
+  // アップロードファイルは20MB以内
+  if (imageList[0].size > serverConfig.app.image_management.maxSize) {
+    errorMessages.push({ filename: imageList[0].name, message: 'image size is too big. : ' + imageList[0].size })
+  }
+
+  // mimetype 対応確認
+  if (!serverConfig.app.image_management.mimeType.includes(imageList[0].mimetype)) {
+    errorMessages.push({ filename: imageList[0].name, message: 'image mime_type is invalid: ' + imageList[0].mimetype })
+  }
+
+  if (errorMessages.length == 0) {
+    // ---
+    // 既存ファイルの削除
+    // ---
+    // 画像ファイルの物理削除
+    if (fs.existsSync(path.join(APP_ROOT, filePath))) {
+      fs.rmSync(path.join(APP_ROOT, filePath));
+    }
 
     // ---
-    // ファイル保存、DB登録
+    // ファイルを保存・配置
     // ---
-    // 単一の場合、配列に変換
-    let imageList = (!Array.isArray(parameters.__files.images)) ? [ parameters.__files.images ] : parameters.__files.images;
-    let bValid = true;
+    // 保存するファイル名は同じファイル名が生じるケースを考えてDate.now()をつけたす
+    let uploadFileExt = path.extname(imageList[0].name);
+    let saveFilename = `${path.basename(imageList[0].name, uploadFileExt)}-${Date.now()}${uploadFileExt}`;
 
-    // アップロードファイルは20MB以内
-    if (imageList[0].size > serverConfig.app.image_management.maxSize) {
-      // consoleLogger.error('imageList[0].size too big! : ', imageList[0].size);
-      bValid = false;
+    // サーバー上の保存位置
+    let savedDir = `${serverConfig.app.image_management.savedImageDir}/${req.user.id}/`;
+    if (!fs.existsSync(path.join(APP_ROOT, 'public', savedDir))) {
+      // 保存先ディレクトリが無い場合は作成
+      fs.mkdirSync(path.join(APP_ROOT, 'public', savedDir), { recursive: true, mode: 0o777 });
     }
+    // file path
+    filePath = `public/${savedDir}${saveFilename}`;
+    // URL path
+    urlPath = `/${savedDir}${saveFilename}`;
+    // original filename
+    imageFilename = imageList[0].name;
+    // mime-type
+    fileMimetype = imageList[0].mimetype;
 
-    // mimetype 対応確認
-    if (!serverConfig.app.image_management.mimeType.includes(imageList[0].mimetype)) {
-      // consoleLogger.error('imageList[0].mimetype bad type! : ', imageList[0].mimetype);
-      bValid = false;
-    }
-
-    if (bValid) {
-      // ---
-      // 既存ファイルの削除
-      // ---
-      // 画像ファイルの物理削除
-      if (fs.existsSync(path.join(APP_ROOT, filePath))) {
-        fs.rmSync(path.join(APP_ROOT, filePath));
-      }
-
-      // ---
-      // ファイルを保存・配置
-      // ---
-      // 保存するファイル名は同じファイル名が生じるケースを考えてDate.now()をつけたす
-      let uploadFileExt = path.extname(imageList[0].name);
-      let saveFilename = `${path.basename(imageList[0].name, uploadFileExt)}-${Date.now()}${uploadFileExt}`;
-
-      // サーバー上の保存位置
-      let savedDir = `${serverConfig.app.image_management.savedImageDir}/${req.user.id}/`;
-      if (!fs.existsSync(path.join(APP_ROOT, 'public', savedDir))) {
-        // 保存先ディレクトリが無い場合は作成
-        fs.mkdirSync(path.join(APP_ROOT, 'public', savedDir), { recursive: true, mode: 0o777 });
-      }
-      filePath = `public/${savedDir}${saveFilename}`;
-      // URLパス
-      urlPath = `/${savedDir}${saveFilename}`;
-      // mime type
-      fileMimetype = imageList[0].mimetype;
-
-      // メモリ上にあるファイルをサーバーパスへ移動させる
-      imageList[0].mv(path.join(APP_ROOT, filePath), (err: any) => {
-        if(err)
-          return res.status(500).send(err);
-      })
-    }
+    // メモリ上にあるファイルをサーバーパスへ移動させる
+    imageList[0].mv(path.join(APP_ROOT, filePath), (err: any) => {
+      if(err)
+        return res.status(500).send(err);
+    })
   }
 
   // ---
@@ -281,7 +295,7 @@ router.post('/:id', isAuthenticated, async function (req, res, next) {
   // 生成データ作成
   const imageData = {
     userId: req.user ? req.user.id : 0,
-    fileName: parameters.name,
+    fileName: imageFilename,
     fileMimetype: fileMimetype,
     filePath: filePath,
     fileUrl: urlPath
@@ -307,9 +321,6 @@ router.get('/:id/delete', isAuthenticated, async function (req, res, next) {
   // パラメータ取得
   const parameters = getAllParameters(req)
   // consoleLogger.debug('parameters', parameters)
-
-  // make an DB connection.
-  // const connection = getConnection()
 
   // DB access
   const image = await dataSource.getRepository(Image).findOneBy({ id: parameters.id })
