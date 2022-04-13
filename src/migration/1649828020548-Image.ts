39003a4d5dc316c61744c39e68f24094a41e26ea
Akinori Nakata <nakata@real-its.com>
2022-04-13 14:35:30 +0900

    entity および migration 再作成

--- src/migration/1649828020548-Image.ts
+++ src/migration/1649828020548-Image.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Image1649259842775 implements MigrationInterface {
-    name = 'Image1649259842775'
+export class Image1649828020548 implements MigrationInterface {
+    name = 'Image1649828020548'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`image\` (\`id\` int NOT NULL AUTO_INCREMENT COMMENT '画像ID', \`userId\` int NOT NULL COMMENT '所有者ユーザーID' DEFAULT '0', \`fileName\` varchar(255) NOT NULL COMMENT 'ファイル名' DEFAULT '', \`fileMimetype\` varchar(255) NOT NULL COMMENT 'ファイルmime-type' DEFAULT '', \`filePath\` varchar(255) NOT NULL COMMENT '物理ファイル相対パス' DEFAULT '', \`fileUrl\` varchar(255) NOT NULL COMMENT '画像URLパス' DEFAULT '', \`createdAt\` timestamp(6) NOT NULL COMMENT '作成日時' DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` timestamp(6) NOT NULL COMMENT '更新日時' DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` timestamp(6) NULL COMMENT '削除日時', PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
